368f2326ad0a0b0ed5a376fe295ea9b45e47a9c9
AS-Sharpdev <assharpdev@aol.com>
2025-08-28 09:02:54 +0200

    Add files via upload

---
+++ Magical Cursor Trails/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magical_Cursor_Trails
+{
+    public partial class Form1 : Form
+    {
+        private List<Trail> trails = new List<Trail>();
+        private Timer timer = new Timer();
+        private static readonly Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += Form1_MouseMove;
+            timer.Interval = 16;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            DoubleBuffered = true;
+
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            trails.Add(new Trail
+            {
+                Position = e.Location,
+                Color = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256))
+            });
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = trails.Count - 1; i >= 0; i--)
+            {
+                trails[i].Life--;
+                if (trails[i].Life <= 0) trails.RemoveAt(i);
+            }
+            this.Invalidate();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            foreach (var t in trails)
+            {
+                using (Brush b = new SolidBrush(Color.FromArgb(t.Life * 8, t.Color)))
+                    e.Graphics.FillEllipse(b, t.Position.X, t.Position.Y, 10, 10);
+            }
+        }
+    }
+   
+}
---
+++ Magical Cursor Trails/Trail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magical_Cursor_Trails
+{
+    public class Trail
+    {
+        public PointF Position;
+        public int Life = 30;
+        public Color Color;
+    }
+}
